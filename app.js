831ad5317878c275ba8e921303a8a2d827efed92
Ahmedu22 <ahmedgithub2236@gmail.com>
2025-05-16 12:24:10 +0300

    switch-assignment

---
+++ app.js
@@ -0,0 +1,14 @@
+let fruit = "Apple";
+switch (fruit) {
+  case "banana":
+    console.log("banana is good");
+    break;
+  case "orage":
+    console.log("am not fan of orage");
+    break;
+  case "Apple":
+    console.log("how you like them Apples");
+    break;
+    default:
+        console.log("i have never heard of that fruit");
+}
